577e0c2ae38e3a8683d3dff06bf558b9751602fd
CupOfEspresso <y.claes@outlook.com>
2018-10-09 22:20:27 +0000

    cleaned up some old scripts

--- neofetch/NeofetchRefresh.sh
+++ neofetch/NeofetchRefresh.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #this script reload the neofetch every few seconds
 
-FETCH="neofetch"
 FLAG=" --ascii_colors 2 2 --colors 2 4 4 4 2 4 --bold on --color_blocks off && printf '\n' "
 
 while true; do
 clear
-$FETCH$FLAG
+neofetch$FLAG
 sleep 120
 done
