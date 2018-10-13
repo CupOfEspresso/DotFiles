5c5998607c6ae9eb145261bea261cebc3ddda7a3
CupOfEspresso <y.claes@outlook.com>
2018-10-13 13:12:57 +0000

    quick change

--- neofetch/NeofetchRefresh.sh
+++ neofetch/NeofetchRefresh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #this script reload the neofetch every few seconds
 
-FLAG=" --ascii_colors 2 2 --colors 2 4 4 4 2 4 --bold on --color_blocks off && printf '\n' "
+#FLAG=" --ascii_colors 2 2 --colors 2 4 4 4 2 4 --bold on --color_blocks off && printf '\n' "
 
 while true; do
 clear
